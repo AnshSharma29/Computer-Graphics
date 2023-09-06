b6ac8558786be2c25326cfcd61e7cd8113e2ea63
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-09-07 00:11:07 +0530

    Create Program 1.c

---
+++ Program 1.c
@@ -0,0 +1,14 @@
+/*How to draw a line*/
+
+#include<graphics.h>
+#include<conio.h>
+
+void main()
+{
+
+  int gd = DETECT, gm;
+  init graph(&gd, &gm, "path of the BGI folder");
+  line(100, 100, 200, 200);
+  getch();
+  closegraph();
+}
